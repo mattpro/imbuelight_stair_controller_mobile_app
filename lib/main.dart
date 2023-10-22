02a2322d36f941f66c7c74090cd8a6de8f4d44b5
Wojciech <wojciech-madeja@wp.pl>
2023-10-22 21:49:56 +0200

    Changed the title app

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'IL Stair Controller',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
