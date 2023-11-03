86af678caf518af08bae99f561f9881f93fe2cdf
Wojciech <wojciech-madeja@wp.pl>
2023-11-03 21:42:39 +0100

    Refactoring

---
+++ lib/methods/font_style.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:imbuelight_stair_controller_mobile_app/enums/enums.dart';
+
+TextStyle fontStyle(Weight weight, double size, Color color, bool isShadow) {
+  if (weight == Weight.light) {
+    if (isShadow) {
+      return TextStyle(
+          fontFamily: 'Comfortaa',
+          fontWeight: FontWeight.w400,
+          fontSize: size,
+          color: color,
+          shadows: [
+            Shadow(
+              color: Colors.black, // Choose the color of the shadow
+              blurRadius: 4.0, // Adjust the blur radius for the shadow effect
+              offset: Offset(1.0, 1.0),
+            )
+          ]);
+    } else {
+      return TextStyle(
+        fontFamily: 'Comfortaa',
+        fontWeight: FontWeight.w400,
+        fontSize: size,
+        color: color,
+      );
+    }
+  } else if (weight == Weight.bold) {
+    if (isShadow) {
+      return TextStyle(
+          fontFamily: 'Comfortaa',
+          fontWeight: FontWeight.w800,
+          fontSize: size,
+          color: color,
+          shadows: [
+            Shadow(
+              color: Colors.black, // Choose the color of the shadow
+              blurRadius: 4.0, // Adjust the blur radius for the shadow effect
+              offset: Offset(1.0, 1.0),
+            )
+          ]);
+    } else {
+      return TextStyle(
+        fontFamily: 'Comfortaa',
+        fontWeight: FontWeight.w800,
+        fontSize: size,
+        color: color,
+      );
+    }
+  } else {
+    if (isShadow) {
+      return TextStyle(
+          fontFamily: 'Comfortaa',
+          fontSize: size,
+          color: color,
+          shadows: [
+            Shadow(
+              color: Colors.black, // Choose the color of the shadow
+              blurRadius: 4.0, // Adjust the blur radius for the shadow effect
+              offset: Offset(1.0, 1.0),
+            )
+          ]);
+    } else {
+      return TextStyle(fontFamily: 'Comfortaa', fontSize: size, color: color);
+    }
+  }
+}
---
+++ lib/pages/sensor_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:imbuelight_stair_controller_mobile_app/controllers/bluetooth_controller.dart';
+import 'package:imbuelight_stair_controller_mobile_app/widges/connection_table.dart';
+
+class SensorPage extends StatelessWidget {
+  const SensorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<BluetoothController>(
+        init: BluetoothController(),
+        builder: (controller) {
+          return Scaffold(
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [ConnectionTable()],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
