3940e496e842069b0376251331d8ac829ad655e4
Wojciech <wojciech-madeja@wp.pl>
2024-01-17 09:05:17 +0100

    Localization in progress

--- lib/controllers/bluetooth_controller.dart
+++ lib/controllers/bluetooth_controller.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'dart:typed_data';
-import 'package:binary/binary.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get/get.dart';
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,7 @@ import 'package:imbuelight_stair_controller_mobile_app/controllers/on_controller
 import 'package:imbuelight_stair_controller_mobile_app/enums/enums.dart';
 import 'package:imbuelight_stair_controller_mobile_app/widges/devices_list.dart';
 import 'package:imbuelight_stair_controller_mobile_app/methods/font_style.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -15,7 +16,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     oc.onReady();
-    
+
     return GetBuilder<BluetoothController>(
         init: BluetoothController(),
         builder: (controller) {
@@ -23,8 +24,8 @@ class HomePage extends StatelessWidget {
             backgroundColor: Color(AppColor.background.value),
             body: SafeArea(
               child: Platform.isMacOS
-                  ?  SingleChildScrollView(
-                    child: Column(
+                  ? SingleChildScrollView(
+                      child: Column(
                         children: [
                           IconButton(
                               onPressed: () async => await _onFresh(controller),
@@ -34,21 +35,26 @@ class HomePage extends StatelessWidget {
                               Padding(
                                 padding: EdgeInsets.symmetric(vertical: 50),
                                 child: Text(
-                                  'Szukaj urządzenia',
+                                  AppLocalizations.of(context)!.searchDevice,
                                   style: fontStyle(
                                       Weight.bold, 27, Colors.white, true),
                                 ),
-                              ), Obx(() => Text('${oc.isOn.value}', style: fontStyle(
-                                  Weight.bold, 27, Colors.white, true))),
-                              Obx(() => oc.isOn.value ? const DevicesList() : Text(
-                              'Bluetooth wyłączony',
-                              style: fontStyle(
-                                  Weight.bold, 27, Colors.white, true))),
+                              ),
+                              Obx(() => Text('${oc.isOn.value}',
+                                  style: fontStyle(
+                                      Weight.bold, 27, Colors.white, true))),
+                              Obx(() => oc.isOn.value
+                                  ? const DevicesList()
+                                  : Text(
+                                      AppLocalizations.of(context)!
+                                          .bluetoothTurnedOff,
+                                      style: fontStyle(Weight.bold, 27,
+                                          Colors.white, true))),
                             ]),
                           ),
                         ],
                       ),
-                  )
+                    )
                   : RefreshIndicator(
                       onRefresh: () => _onFresh(controller),
                       child: SingleChildScrollView(
@@ -56,7 +62,7 @@ class HomePage extends StatelessWidget {
                           Padding(
                             padding: EdgeInsets.symmetric(vertical: 50),
                             child: Text(
-                              'Szukaj urządzenia',
+                              AppLocalizations.of(context)!.searchDevice,
                               style: fontStyle(
                                   Weight.bold, 27, Colors.white, true),
                             ),
@@ -67,9 +73,7 @@ class HomePage extends StatelessWidget {
                     ),
             ),
           );
-        })
-        ;
-
+        });
   }
 }
 
